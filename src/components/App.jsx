b83adebbe6960d7b49b638dc67d750bcbf403f9f
Dmitry Skripunov <dskripunov@gmail.com>
2017-03-07 11:15:40 +0300

    add view mode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,121 @@
 ﻿'use strict';
 
 import React from 'react';
-import parser from './libs/parser';
+import {parser, makeJSON, makeHTML, makeJHTML, syntaxHighlight} from '../libs/parser';
 
 class App extends React.Component {
 	shouldComponentUpdate(nextProps, nextState) {
+
 		return true;
+
 	}
 
 	constructor(props) {
+
 		super(props);
 		this.state = {
-			defaulJson: '{"address":{"streetAddress":"21 2nd Street","city":"New York"},"phoneNumber":[{"location":"home","code":44, "test":{"first": 1, "two":null}}]}',
-			defaulSchema: '{"title":"Schema","type":"object","description":"test json","properties":[{"description":"","title":"address","type":"object","properties":[{"description":"","title":"streetAddress","type":"string","default":"21 2nd Street"},{"description":"","title":"city","type":"string","default":"New York"}]},{"description":"","title":"phoneNumber","type":"array","properties":[{"description":"","type":"object","properties":[{"description":"","title":"location","type":"string","default":"home"},{"description":"","title":"code","type":"number","default":44},{"description":"","title":"test","type":"object","properties":[{"description":"","title":"first","type":"number","default":1},{"description":"","title":"two","type":"object","default":null}]}]}]}]}'
+			inputJson: '{"address":{"streetAddress":"21 2nd Street","city":"New York"},"phoneNumber":[{"location":"home","code":44, "test":{"first": 1, "two":null}}]}',
+			inputSchema: '{"title":"Schema","type":"object","description":"test json","properties":[{"description":"","title":"address","type":"object","properties":[{"description":"","title":"streetAddress","type":"string","default":"21 2nd Street"},{"description":"","title":"city","type":"string","default":"New York"}]},{"description":"","title":"phoneNumber","type":"array","properties":[{"description":"","type":"object","properties":[{"description":"","title":"location","type":"string","default":"home"},{"description":"","title":"code","type":"number","default":44},{"description":"","title":"test","type":"object","properties":[{"description":"","title":"first","type":"number","default":1},{"description":"","title":"two","type":"object","default":null}]}]}]}]}',
+			result: ''
 		};
+
+		this.changeInputJSON 		= this.changeInputJSON.bind(this);
+		this.changeInputSchema 	= this.changeInputSchema.bind(this);
+		this.makeJSON 					= this.makeJSON.bind(this);
+		this.makeSchema 				= this.makeSchema.bind(this);
+		this.makeHTML 					= this.makeHTML.bind(this);
+		this.makeJHTML 					= this.makeJHTML.bind(this);
+
 	}
 
 	render() {
-		return <div className="input-block">
-						<div>
-							<h2>Load JSON</h2>
-							<textarea defaultValue={this.state.defaultJson} />
-							<div>
-								<button onClick={this.makeHTML()}>Make HTML</button>
-								<button onClick="makeJHTML4()">Make JHTML</button>
-								<button onClick="make2()">Make JSON</button>
-								<button onClick="make3()">Make Schema</button>
-							</div>
-						</div>
 
-						<div>
-							<h2>Load schema</h2>
-							<textarea defaultValue={this.state.defaulSchema} />
-							<div>
-								<button onClick="make(true)">Make HTML</button>
-								<button onClick="makeJHTML4(true)">Make JHTML</button>
-								<button onClick="make2(true)">Make JSON</button>
-								<button onClick="make3(true)">Make Schema</button>
-							</div>
-						</div>
+		return <div>
+							<div className="input-block">
+									<div>
+										<h2>Load JSON</h2>
+										<textarea value={this.state.inputJson} onChange={this.changeInputJSON} />
+										<div>
+											<button onClick={this.makeJSON} value="json">Make JSON</button>
+											<button onClick={this.makeSchema} value="json">Make Schema</button>
+											<button onClick={this.makeHTML} value="json">Make HTML</button>
+											<button onClick={this.makeJHTML} value="json">Make JHTML</button>
+										</div>
+									</div>
 
-					<h2>Results</h2>
-					<div id="results"></div>
+									<div>
+										<h2>Load schema</h2>
+										<textarea value={this.state.inputSchema} onChange={this.changeInputSchema} />
+										<div>
+											<button onClick={this.makeJSON} value="schema">Make JSON</button>
+											<button onClick={this.makeSchema} value="schema">Make Schema</button>
+											<button onClick={this.makeHTML} value="schema">Make HTML</button>
+											<button onClick={this.makeJHTML} value="schema">Make JHTML</button>
+										</div>
+									</div>
+							</div>
+
+							<h2>Results</h2>
+							<div id="results" dangerouslySetInnerHTML={{__html: this.state.result}}></div>
 
 					</div>;
+
 	}
 
-	makeHTML(){
-		const testJSON = document.querySelector(`#${isSchema ? 'schemaInput' : 'jsonInput'}`).value;
+	changeInputJSON(evt) {
 
-		schema = isSchema ? JSON.parse(testJSON) : parser(testJSON, "test json");
+		this.setState({inputJson: evt.target.value});
 
-		if(schema !== null){
-			var makedHTML = makeHTML(schema);
-			if(makedHTML !== null)
-				document.querySelector('#results').innerHTML = makedHTML;
-		}
+	}
+
+	changeInputSchema(evt) {
+
+		this.setState({inputSchema: evt.target.value});
+
+	}
+
+	makeJSON(evt) {
+
+		const testJSON = evt.target.value === 'json' ? this.state.inputJson : this.state.inputSchema;
+
+		const schema = evt.target.value !== 'json' ? JSON.parse(testJSON) : parser(testJSON, 'test json');
+
+		if(schema !== null)
+			this.setState({result: `<pre>${syntaxHighlight(JSON.stringify(makeJSON(schema), undefined, 4))}</pre>`});
+
+	}
+
+	makeSchema(evt) {
+
+		const testJSON = evt.target.value === 'json' ? this.state.inputJson : this.state.inputSchema;
+
+		const schema = evt.target.value !== 'json' ? JSON.parse(testJSON) : parser(testJSON, 'test json');
+
+		if(schema !== null)
+			this.setState({result: JSON.stringify(schema)});
+
+	}
+
+	makeHTML(evt) {
+
+		const testJSON = evt.target.value === 'json' ? this.state.inputJson : this.state.inputSchema;
+
+		const schema = evt.target.value !== 'json' ? JSON.parse(testJSON) : parser(testJSON, 'test json');
+
+		if(schema !== null)
+			this.setState({result: makeHTML(schema)});
+
+	}
+
+	makeJHTML(evt) {
+
+		const testJSON = evt.target.value === 'json' ? this.state.inputJson : this.state.inputSchema;
+
+		const schema = evt.target.value !== 'json' ? JSON.parse(testJSON) : parser(testJSON, 'test json');
+
+		if(schema !== null)
+			this.setState({result: makeJHTML(schema)});
+
 	}
 
 }
--- src/libs/parser.js
+++ src/libs/parser.js
@@ -30,37 +30,50 @@
  *
  *
  */
-export function parser(json, desc){
+export function parser(json, desc) {
+
 	const schema = {
-		"title": "Schema",
-		"type": null,
-		"description": ""
+		'title': 'Schema',
+		'type': null,
+		'description': ''
 	};
 
 	try {
+
 		json = JSON.parse(json);
-	}catch(ex){
+
+	} catch (ex) {
+
 		return null;
+
 	}
 
 	schema.description = desc.toString();
 
-	if (Array.isArray(json)) {
-		schema.type = "array";
+	if(Array.isArray(json)) {
+
+		schema.type = 'array';
 		schema.properties = [];
 		json.forEach((p, i) => {
+
 			schema.properties.push(_convertProperty(p));
+
 		});
-	}
-	else {
+
+	} else {
+
 		schema.properties = [];
-		schema.type = "object";
-		for (let key in json) {
+		schema.type = 'object';
+		for(const key in json) {
+
 			schema.properties.push(_convertProperty(json[key], key));
+
 		}
+
 	}
 
 	return schema;
+
 }
 
 /**
@@ -70,133 +83,184 @@ export function parser(json, desc){
  * @returns
  * @private
  */
-export function _convertProperty(prop, propName){
+export function _convertProperty(prop, propName) {
+
 	const cProp = {
 		'description': ''
 	};
 
-	if(propName !== undefined){
+	if(propName !== undefined) {
+
 		cProp.title = propName;
+
 	}
 
-	if(typeof prop === 'object'){
-		if(Array.isArray(prop)){
+	if(typeof prop === 'object') {
+
+		if(Array.isArray(prop)) {
+
 			cProp.type = 'array';
 			cProp.properties = [];
 			prop.forEach((p, i) => {
+
 				cProp.properties.push(_convertProperty(p));
-		});
-		}
-		else {
+
+			});
+
+		}	else {
+
 			cProp.type = 'object';
-			if(prop === null){
+			if(prop === null) {
+
 				cProp.default = prop;
-			}
-			else{
+
+			} else {
+
 				cProp.properties = [];
-				for(let key in prop){
+				for(const key in prop) {
+
 					cProp.properties.push(_convertProperty(prop[key], key));
+
 				}
+
 			}
+
 		}
 
-	}
-	else{
+	} else {
+
 		cProp.type = typeof prop;
 		cProp.default = prop;
+
 	}
 
 	return cProp;
+
 }
 
-export function makeJSON(schema){
+export function makeJSON(schema) {
+
 	let json = null;
-	if(schema.type === 'object'){
+	if(schema.type === 'object') {
+
 		json = {};
-		schema.properties.forEach((item) => {
+		schema.properties.forEach(item => {
+
 			json[item.title] = _makeJSONProp(item);
+
 		});
-	}
-	else{
+
+	}	else {
+
 		json = [];
-		schema.properties.forEach((item) => {
+		schema.properties.forEach(item => {
+
 			json.push(_makeJSONProp(item));
+
 		});
+
 	}
 
 	return json;
+
 }
 
-export function _makeJSONProp(prop){
+export function _makeJSONProp(prop) {
+
 	let p = null;
 
-	if(prop.type === 'object' && prop.hasOwnProperty('default')){
+	if(prop.type === 'object' && prop.hasOwnProperty('default')) {
+
 		p = prop.default;
-	}
-	else if(prop.type === 'object'){
+
+	}	else if(prop.type === 'object') {
+
 		p = {};
-		prop.properties.forEach((item) => {
+		prop.properties.forEach(item => {
+
 			p[item.title] = _makeJSONProp(item);
+
 		});
-	}
-	else if(prop.type === 'array') {
+
+	}	else if(prop.type === 'array') {
+
 		p = [];
-		prop.properties.forEach((item) => {
+		prop.properties.forEach(item => {
+
 			p.push(_makeJSONProp(item));
+
 		});
-	}
-	else{
+
+	}	else {
+
 		p = prop.default;
+
 	}
 
 	return p;
+
 }
 
-export function _randomString(){
-	var text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+export function _randomString() {
 
-	for( var i=0; i < 5; i++ )
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for(let i = 0; i < 5; i++)
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 
 	return text;
+
 }
 
 export function syntaxHighlight(json) {
-	if (typeof json != 'string') {
+
+	if(typeof json !== 'string')
 		json = JSON.stringify(json, undefined, 2);
-	}
+
 	json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-		var cls = 'number';
-		if (/^"/.test(match)) {
-			if (/:$/.test(match)) {
+	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, match => {
+
+		let cls = 'number';
+		if(/^"/.test(match)) {
+
+			if(/:$/.test(match))
 				cls = 'key';
-			} else {
+			else
 				cls = 'string';
-			}
-		} else if (/true|false/.test(match)) {
+
+		} else if(/true|false/.test(match)) {
+
 			cls = 'boolean';
-		} else if (/null/.test(match)) {
+
+		} else if(/null/.test(match)) {
+
 			cls = 'null';
+
 		}
-		return '<span class="' + cls + '">' + match + '</span>';
+		return `<span class=${cls}>${match}</span>`;
+
 	});
+
 }
 
-export function makeHTML(schema, isRoot = true){
+export function makeHTML(schema, isRoot = true) {
+
 	let html = '';
 
 	const date = _randomString();
 
-	if(schema.type === 'object' && schema.hasOwnProperty('default')){
+	if(schema.type === 'object' && schema.hasOwnProperty('default')) {
+
 		html += schema.default;
-	}
-	else if(schema.type === 'object' || schema.type === 'array'){
+
+	}	else if(schema.type === 'object' || schema.type === 'array') {
 
 		if(!isRoot) {
+
 			html += `<input type="checkbox" class="table-view-switcher" id="switcher-${date}">`;
 			html += `<label for="switcher-${date}" class="${isRoot ? 'root' : ''}"></label>`;
+
 		}
 
 		html += `<table class="object ${!isRoot ? 'hide' : ''}" cellpadding="0" cellspacing="0" >`;
@@ -204,7 +268,7 @@ export function makeHTML(schema, isRoot = true){
 		html += '<tr>';
 		html += `<td class="object-header" colspan="2">${(schema.type === 'object') ? 'Object' : 'Array'} {${schema.properties.length}}</td>`;
 		html += `<td>${schema.description}</td>`;
-		html += `</tr>`;
+		html += '</tr>';
 
 		html += '<tr class="header">';
 		html += `<td>${schema.type === 'array' ? 'Item #' : 'Key'}</td>`;
@@ -213,117 +277,140 @@ export function makeHTML(schema, isRoot = true){
 		html += '</tr>';
 
 		schema.properties.forEach((prop, i) => {
+
 			html += `<tr class="row"><td class="object-key">${(schema.type === 'object') ? prop.title : i}</td>`;
 			html += `<td class="object-value">${makeHTML(prop, false)}</td>`;
 			html += `<td class="object-description">${prop.description}</td>`;
-			html += `</tr>`;
+			html += '</tr>';
+
 		});
 
-		html +='</table>';
+		html += '</table>';
 
-	}
-	else{
+	}	else
 		html += schema.default;
-	}
-
 
 	return html;
+
 }
 
-export function makeJHTML(schema, isRoot = true, level = 1){
+export function makeJHTML(schema, isRoot = true, level = 1) {
+
 	let html = '';
 
-	if((schema.type === 'object' || schema.type === 'array') && !schema.hasOwnProperty('default')){
+	if((schema.type === 'object' || schema.type === 'array') && !schema.hasOwnProperty('default')) {
 
 		if(isRoot) {
+
 			html += `<span class="key-postfix">${(schema.type === 'object') ? `Object {${schema.properties.length}}` : `Array [${schema.properties.length}]`}</span>`;
 			html += _makeDescHandler(schema);
 
-			_makeEditMenu();
 		}
 
-		html += `<div class="prop" style="margin-left:${level*10}px">`;
+		html += `<div class="prop" style="margin-left:${level * 10}px">`;
 
 		++level;
 		schema.properties.forEach((prop, i) => {
-			html += `<div style="margin: 15px 0;">`;
+
+			html += '<div style="margin: 15px 0;">';
 
 			if((prop.type === 'object' || prop.type === 'array') && !prop.hasOwnProperty('default')) {
+
 				const id = _randomString();
 				html += `<input type="checkbox" class="jhtml-view-switcher" id="jhtml-view-switcher-${id}"/>`;
 				html += `<label for="jhtml-view-switcher-${id}"></label>`;
+
 			}
 
 
 			let v = (schema.type === 'object') ? prop.title : i;
 			let cl = '';
-			if(v === ''){
+			if(v === '') {
+
 				cl += ' empty';
 				v = '(empty string)';
+
 			}
 			html += `<span class="${cl}">${v}</span>`;
 
-			if(prop.type === 'object' && !prop.hasOwnProperty('default')){
+			if(prop.type === 'object' && !prop.hasOwnProperty('default')) {
+
 				html += ` <span class="key-postfix">{${prop.properties.length}}</span>`;
-			}
-			else if(prop.type === 'array' && !prop.hasOwnProperty('default')){
+
+			} else if(prop.type === 'array' && !prop.hasOwnProperty('default')) {
+
 				html += ` <span class="key-postfix">[${prop.properties.length}]</span>`;
-			}else {
-				html += ` <span class="key-postfix">:</span> `;
+
+			} else {
+
+				html += ' <span class="key-postfix">:</span> ';
+
 			}
 
-			if((prop.type === 'object' || prop.type === 'array') && !prop.hasOwnProperty('default')) {
+			if((prop.type === 'object' || prop.type === 'array') && !prop.hasOwnProperty('default'))
 				html += _makeDescHandler(prop);
-			}
 
 			html += makeJHTML(prop, false, level);
-			html += `</div>`;
+			html += '</div>';
+
 		});
 
-		html += `</div>`;
+		html += '</div>';
 
 		/*if(isRoot)
 			html += `<div>${(schema.type === 'object') ? '}' : ']'}</div>`;*/
 
-	}
-	else{
+	} else {
+
 		let cl = schema.type === 'object' ? 'null' : schema.type;
 		let v = schema.default;
-		if(schema.default === ''){
+		if(schema.default === '') {
+
 			cl += ' empty';
 			v = '(empty string)';
+
 		}
 		
 		html += `<span class="${cl}">${v}</span>`;
 		html += _makeDescHandler(schema);
+
 	}
 
-	function _makeDescHandler(prop){
+	function _makeDescHandler(prop) {
+
 		let html = '';
-		html += `<div class="desc-handler">?`;
+		html += '<div class="desc-handler">?';
 		html += `<div class="desc">${_makeDescription(prop)}</div>`;
-		html += `</div>`;
+		html += '</div>';
 		return html;
+
 	}
 
-	function _makeDescription(prop){
+	function _makeDescription(prop) {
+
 		return prop.description.replace(/(?:\r\n|\r|\n)/g, '<br />');
+
 	}
 
 	return html;
+
 }
 
-export function makeEditableJHTML(schema, isRoot = true, level = 1, parent){
+export function makeEditableJHTML(schema, isRoot = true, level = 1, parent) {
+
 	let html;
-	if(parent !== undefined){
+	if(parent !== undefined) {
+
 		html = parent;
-	}
-	else{
+
+	}	else {
+
 		html = document.createElement('div');
 		html.className = 'editmode';
+
 	}
 
-	if((schema.type === 'object' || schema.type === 'array') && !schema.hasOwnProperty('default')){
+	if((schema.type === 'object' || schema.type === 'array') && !schema.hasOwnProperty('default')) {
 
 		if(isRoot) {
 
@@ -334,20 +421,25 @@ export function makeEditableJHTML(schema, isRoot = true, level = 1, parent){
 			postfix.innerHTML = (schema.type === 'object') ? `Object {${schema.properties.length}}` : `Array [${schema.properties.length}]`;
 			html.appendChild(postfix);
 			html.appendChild(_makeDescHandler(schema));
+
+			_makeEditMenu();
+
 		}
 
 		const propBlock = document.createElement('div');
 		propBlock.className = 'prop';
-		propBlock.style = `margin-left:${level*10}px`;
+		propBlock.style = `margin-left:${level * 10}px`;
 
 		++level;
 		schema.properties.forEach((prop, i) => {
+
 			const propNode = document.createElement('div');
-			propNode.style = "margin: 15px 0;";
+			propNode.style = 'margin: 15px 0;';
 
 			propNode.appendChild(_makeEditHandler(prop, level, propNode));
 
 			if((prop.type === 'object' || prop.type === 'array') && !prop.hasOwnProperty('default')) {
+
 				const id = _randomString();
 
 				const switcher = document.createElement('input');
@@ -359,29 +451,39 @@ export function makeEditableJHTML(schema, isRoot = true, level = 1, parent){
 				const switcherLabel = document.createElement('label');
 				switcherLabel.setAttribute('for', `jhtml-view-switcher-${id}`);
 				propNode.appendChild(switcherLabel);
+
 			}
 
 			const key = document.createElement('span');
 			if(schema.type !== 'array') {
+
 				key.contentEditable = true;
 				key.setAttribute('placeholder', '(empty string)');
 				key.className = 'editablekey';
-				key.onkeyup = function (evt) {
-					if(_isKeyDuplicate(evt.currentTarget.innerText, schema.properties, i)){
+				key.onkeyup = evt => {
+
+					if(_isKeyDuplicate(evt.currentTarget.innerText, schema.properties, i)) {
+
 						key.title = 'The key is duplicated!';
 						key.classList.add('keyerror');
-					}else {
+
+					} else {
+
 						key.classList.remove('keyerror');
 						key.title = '';
+
 					}
 					prop.title = evt.currentTarget.innerText;
+
 				};
 
-				key.onkeydown = function(evt){
-					if(evt.keyCode === 13){
+				key.onkeydown = evt => {
+
+					if(evt.keyCode === 13)
 						evt.preventDefault();
-					}
+
 				};
+
 			}
 			key.innerHTML = `${(schema.type === 'object') ? prop.title : i}`;
 
@@ -390,77 +492,84 @@ export function makeEditableJHTML(schema, isRoot = true, level = 1, parent){
 			const postfix = document.createElement('span');
 			postfix.className = 'key-postfix';
 
-			if(prop.type === 'object' && !prop.hasOwnProperty('default')){
+			if(prop.type === 'object' && !prop.hasOwnProperty('default'))
 				postfix.innerHTML = ` {${prop.properties.length}}`;
-			}
-			else if(prop.type === 'array' && !prop.hasOwnProperty('default')){
+			else if(prop.type === 'array' && !prop.hasOwnProperty('default'))
 				postfix.innerHTML = ` [${prop.properties.length}]`;
-			}else {
-				postfix.innerHTML = ` : `;
-			}
+			else
+				postfix.innerHTML = ' : ';
 
 			propNode.appendChild(postfix);
 
-
-			if((prop.type === 'object' || prop.type === 'array') && !prop.hasOwnProperty('default')) {
+			if((prop.type === 'object' || prop.type === 'array') && !prop.hasOwnProperty('default'))
 				propNode.appendChild(_makeDescHandler(prop));
-			}
 
 			makeEditableJHTML(prop, false, level, propNode);
 
 			propBlock.appendChild(propNode);
+
 		});
 
 		html.appendChild(propBlock);
 
-	}
-	else{
-		let cl = schema.type === 'object' ? 'null' : schema.type;
+	}	else {
+
+		const cl = schema.type === 'object' ? 'null' : schema.type;
 
 		const value = document.createElement('span');
-		value.className = cl + ' editablekey';
+		value.className = `${cl} editablekey`;
 		value.contentEditable = true;
 		value.setAttribute('placeholder', '(empty string)');
-		value.onkeyup = function(evt){
+		value.onkeyup = evt => {
+
 			const putValue =  evt.currentTarget.innerText;
 			/**
 			 * value can be: number, or boolean, or string, or null
 			 * @type {string}
 			 */
-			if(putValue === 'null'){
+			if(putValue === 'null') {
+
 				schema.default = null;
 				schema.type = 'object';
-			}
-			else if(putValue === 'true' || putValue === 'false'){
+
+			}	else if(putValue === 'true' || putValue === 'false') {
+
 				schema.default = putValue === 'true';
 				schema.type = 'boolean';
-			}
-			else if(isNumeric(putValue)){
+
+			}	else if(isNumeric(putValue)) {
+
 				schema.default = parseFloat(putValue);
 				schema.type = 'number';
-			}
-			else{
+
+			}	else {
+
 				schema.default = putValue;
 				schema.type = 'string';
+
 			}
 
-			let cl = schema.type === 'object' ? 'null' : schema.type;
-			value.className = cl + ' editablekey';
+			const cl = schema.type === 'object' ? 'null' : schema.type;
+			value.className = `${cl} editablekey`;
+
 		};
 
-		value.onkeydown = function(evt){
-			if(evt.keyCode === 13){
+		value.onkeydown = function(evt) {
+
+			if(evt.keyCode === 13)
 				evt.preventDefault();
-			}
+
 		};
 
 		value.innerHTML = (schema.default === null) ?  'null' : schema.default;
 
 		html.appendChild(value);
 		html.appendChild(_makeDescHandler(schema));
+
 	}
 
-	function _makeDescHandler(prop){
+	function _makeDescHandler(prop) {
+
 		const descHandler = document.createElement('div');
 		descHandler.className = 'desc-handler';
 		descHandler.innerHTML = '?';
@@ -469,38 +578,45 @@ export function makeEditableJHTML(schema, isRoot = true, level = 1, parent){
 		desc.className = 'desc';
 		desc.appendChild(_makeDescription(prop));
 
-
 		descHandler.appendChild(desc);
 
 		return descHandler;
+
 	}
 
-	function _makeDescription(prop){
+	function _makeDescription(prop) {
+
 		const d = document.createElement('div');
 		d.className = 'editablekey';
 		d.contentEditable = true;
 		d.innerHTML = prop.description.replace(/(?:\r\n|\r|\n)/g, '<br />');
-		d.onkeyup = function(evt){
+		d.onkeyup = evt => {
+
 			prop.description = evt.currentTarget.innerText;
+
 		};
 		return d;
+
 	}
 
 	return html;
+
 }
 
-export function _makeEditHandler(prop, level, propNode){
+export function _makeEditHandler(prop, level, propNode) {
+
 	const d = document.createElement('div');
 	d.className = 'edithandler';
 
-	d.onclick = function(evt){
+	d.onclick = function(evt) {
+
 		evt.stopPropagation();
 
 		const menu = document.querySelector('#edit-menu');
 
 		menu.style.display = 'block';
-		menu.style.left = evt.currentTarget.offsetLeft + 'px';
-		menu.style.top = evt.currentTarget.offsetTop + 'px';
+		menu.style.left = `${evt.currentTarget.offsetLeft}px`;
+		menu.style.top = `${evt.currentTarget.offsetTop}px`;
 
 		/**
 		 * <li>Insert Array</li>
@@ -509,8 +625,9 @@ export function _makeEditHandler(prop, level, propNode){
 		 <li>Duplicate</li>
 		 <li>Remove</li>
 		 */
-		menu.children[0].onclick = function(){
-			let newProp = {
+		menu.children[0].onclick = function() {
+
+			const newProp = {
 				'type': 'array',
 				'title': '',
 				'description': '',
@@ -521,46 +638,59 @@ export function _makeEditHandler(prop, level, propNode){
 
 			makeEditableJHTML(newProp, false, level, propNode.lastChild);
 			menu.style.display = 'none';
+
 		};
 
 
 		if((prop.type === 'object' || prop.type === 'array') && !prop.hasOwnProperty('default')) {
+
 			menu.children[0].style.display = 'block';
 			menu.children[1].style.display = 'block';
 			menu.children[2].style.display = 'block';
 
-		}else{
+		} else {
+
 			menu.children[0].style.display = 'none';
 			menu.children[1].style.display = 'none';
 			menu.children[2].style.display = 'none';
+
 		}
 
 	};
 
-
 	return d;
+
 }
 
-export function _makeEditMenu(){
+export function _makeEditMenu() {
+
 	const r = document.querySelector('#results');
 
-	r.onclick = function(){
+	r.onclick = function() {
+
 		const menu = document.querySelector('#edit-menu');
 		menu.style.display = 'none';
-	}
+
+	};
 
 }
 
 export function isNumeric(n) {
+
 	return !isNaN(parseFloat(n)) && isFinite(n);
+
 }
 
-export function _isKeyDuplicate(key, props, keyIndex){
+export function _isKeyDuplicate(key, props, keyIndex) {
+
 	let isDuplicate = false;
 
 	props.forEach((prop, i) => {
+
 		if(prop.title === key && i !== keyIndex) isDuplicate = true;
+
 	});
 
 	return isDuplicate;
+
 }
